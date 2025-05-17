f24509f359d40d9859a31cad8178deb75466fe29
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-05-17 00:28:23 -0300

    feat: finalizar projeto com padrões Singleton, Strategy e Facade
    - Implementado padrão Singleton via @Autowired para injeção de dependência.
    - Interface Cliente utiliza o padrão Strategy para comportamento dinâmico.
    - Aplicado padrão Facade para encapsular lógica de persistência de dados do cliente.
    - Integração com API externa para busca de CEP durante o salvamento dos dados.

--- src/main/java/com/mateus/lima/design_patterns_spring/controller/ClienteRestController.java
+++ src/main/java/com/mateus/lima/design_patterns_spring/controller/ClienteRestController.java
@@ -2,6 +2,7 @@ package com.mateus.lima.design_patterns_spring.controller;
 
 import com.mateus.lima.design_patterns_spring.models.ClienteEntity;
 import com.mateus.lima.design_patterns_spring.useCases.ClienteUseCase;
+import com.mateus.lima.design_patterns_spring.useCases.interfaces.ClienteInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,33 +11,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("clientes")
 public class ClienteRestController {
     @Autowired
-    private ClienteUseCase clienteUseCase;
+    private ClienteInterface clienteInterface;
 
     @GetMapping
     public ResponseEntity<Iterable<ClienteEntity>> buscarTodos(){
-        return ResponseEntity.ok(clienteUseCase.buscarTodos());
+        return ResponseEntity.ok(clienteInterface.buscarTodos());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ClienteEntity> buscarPorId(@PathVariable Long id){
-        return ResponseEntity.ok(clienteUseCase.buscarPorId(id));
+        return ResponseEntity.ok(clienteInterface.buscarPorId(id));
     }
 
-    @PatchMapping
+    @PostMapping
     public ResponseEntity<ClienteEntity> inserir(@RequestBody ClienteEntity clienteEntity){
-        clienteUseCase.inserir(clienteEntity);
+        clienteInterface.inserir(clienteEntity);
         return ResponseEntity.ok(clienteEntity);
     }
 
     @PutMapping("/{id}")
     public  ResponseEntity<ClienteEntity> atualizar(@PathVariable Long id, @RequestBody ClienteEntity clienteEntity){
-        clienteUseCase.atualizar(id,clienteEntity);
+        clienteInterface.atualizar(id,clienteEntity);
         return ResponseEntity.ok(clienteEntity);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
-        clienteUseCase.deletar(id);
+        clienteInterface.deletar(id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/mateus/lima/design_patterns_spring/useCases/ClienteUseCase.java
+++ src/main/java/com/mateus/lima/design_patterns_spring/useCases/ClienteUseCase.java
@@ -1,35 +1,67 @@
 package com.mateus.lima.design_patterns_spring.useCases;
 
 import com.mateus.lima.design_patterns_spring.models.ClienteEntity;
+import com.mateus.lima.design_patterns_spring.models.EnderecoEntity;
+import com.mateus.lima.design_patterns_spring.repositories.ClienteRepository;
+import com.mateus.lima.design_patterns_spring.repositories.EnderecoRepository;
 import com.mateus.lima.design_patterns_spring.useCases.interfaces.ClienteInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteUseCase implements ClienteInterface {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private  ViaCepUseCase viaCepUseCase;
 
     @Override
     public Iterable<ClienteEntity> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public ClienteEntity buscarPorId(Long id) {
-        return null;
+        Optional<ClienteEntity> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(ClienteEntity cliente) {
+        cadastraClienteComCep(cliente);
 
     }
 
     @Override
     public void atualizar(Long id, ClienteEntity cliente) {
 
+        Optional<ClienteEntity> clienteCadastrado = clienteRepository.findById(id);
+        if(clienteCadastrado.isPresent()){
+            cadastraClienteComCep(cliente);
+        }
+
     }
 
     @Override
     public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
 
+    private void cadastraClienteComCep(ClienteEntity cliente){
+        String cep = cliente.getEndereco().getCep();
+        EnderecoEntity endereco = enderecoRepository.findById(cep).orElseGet(() ->{
+            EnderecoEntity novoEndereco = viaCepUseCase.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 }
